1e8356789abca6ada384a5b1068a89361d9a1d50
mahlukganteng <57159041+mahlukganteng@users.noreply.github.com>
2019-12-02 09:03:08 +0700

    Update gojek.php

--- gojek.php
+++ gojek.php
@@ -1,6 +1,6 @@
 <?php date_default_timezone_set('Asia/Jakarta');
 include "function.php";
-echo color("green","[]  Gunakan awalan 62      []\n");
+echo color("green","[]  Gunakan awalan 62      []\n");v
 echo color("green","[]  Time  : ".date('[d-m-Y] [H:i:s]')."   []\n");
 
 function change(){
